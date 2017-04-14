e9e9aa6da0f538bec8254db957fff5ae8c67a2ba
Damien Frémont <damien.fremont@gmail.com>
2017-04-14 16:03:05 +0200

    fix 3 - thrust

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -3,32 +3,46 @@ import java.io.*;
 import java.math.*;
 
 /**
- * This code automatically collects game data in an infinite loop.
- * It uses the standard input to place data into the game variables such as x and y.
- * YOU DO NOT NEED TO MODIFY THE INITIALIZATION OF THE GAME VARIABLES.
+ * Auto-generated code below aims at helping you parse the standard input
+ * according to the problem statement.
  **/
 class Player {
 
-    public static void main(String args[]) {
-        Scanner in = new Scanner(System.in);
+	public static void main(String args[]) {
+		Scanner in = new Scanner(System.in);
 
-        // game loop
-        while (true) {
-            int x = in.nextInt(); // x position of your pod
-            int y = in.nextInt(); // y position of your pod
-            int nextCheckpointX = in.nextInt(); // x position of the next check point
-            int nextCheckpointY = in.nextInt(); // y position of the next check point
+		// game loop
+		while (true) {
+			int x = in.nextInt();
+			int y = in.nextInt();
+			int nextCheckpointX = in.nextInt(); // x position of the next check
+												// point
+			int nextCheckpointY = in.nextInt(); // y position of the next check
+												// point
+			int nextCheckpointDist = in.nextInt(); // distance to the next
+													// checkpoint
+			int nextCheckpointAngle = in.nextInt(); // angle between your pod
+													// orientation and the
+													// direction of the next
+													// checkpoint
+			int opponentX = in.nextInt();
+			int opponentY = in.nextInt();
 
-            // Write an action using System.out.println()
-            // To debug: System.err.println("Debug messages...");
+			// thrust algo
+			int thrust = 0;
+			if ((nextCheckpointAngle > 90) || (nextCheckpointAngle < -90)) {
+				thrust = 0;
+			} else {
+				thrust = 100;
+			}
 
+			// Write an action using System.out.println()
+			// To debug: System.err.println("Debug messages...");
 
-            // Edit this line to output the target position
-            // and thrust (0 <= thrust <= 100)
-            // i.e.: "x y thrust"
-            System.out.println(nextCheckpointX + " " + nextCheckpointY + " 100");
-
-
-        }
-    }
+			// You have to output the target position
+			// followed by the power (0 <= thrust <= 100)
+			// i.e.: "x y thrust"
+			System.out.println(nextCheckpointX + " " + nextCheckpointY + " " + thrust);
+		}
+	}
 }
