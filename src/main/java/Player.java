0363b334d725bc44354025c92085293758a0a95e
Damien Frémont <damien.fremont@gmail.com>
2017-04-14 18:26:12 +0200

    add java project

---
+++ src/main/java/Player.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Player {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+
+        // game loop
+        while (true) {
+            int myShipCount = in.nextInt(); // the number of remaining ships
+            int entityCount = in.nextInt(); // the number of entities (e.g. ships, mines or cannonballs)
+            for (int i = 0; i < entityCount; i++) {
+                int entityId = in.nextInt();
+                String entityType = in.next();
+                int x = in.nextInt();
+                int y = in.nextInt();
+                int arg1 = in.nextInt();
+                int arg2 = in.nextInt();
+                int arg3 = in.nextInt();
+                int arg4 = in.nextInt();
+            }
+            for (int i = 0; i < myShipCount; i++) {
+
+                // Write an action using System.out.println()
+                // To debug: System.err.println("Debug messages...");
+
+                System.out.println("MOVE 11 10"); // Any valid action, such as "WAIT" or "MOVE x y"
+            }
+        }
+    }
+}
