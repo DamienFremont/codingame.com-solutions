e14834e8004a5eb9797cf19346a95b780ec687d0
Damien Frémont <damien.fremont@gmail.com>
2017-04-16 15:41:51 +0200

    add replay datas

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,44 +1,14 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 class Player {
 
 	public static void main(String args[]) {
 		Scanner in = new Scanner(System.in);
-		Log.debug("inputInit-----");
-		int factoryCount = in.nextInt(); // the number of factories
-		int linkCount = in.nextInt(); // the number of links between factories
-		Log.debug("factoryCount = %d", factoryCount);
-		Log.debug("linkCount = %d", linkCount);
-		for (int i = 0; i < linkCount; i++) {
-			int factory1 = in.nextInt();
-			int factory2 = in.nextInt();
-			int distance = in.nextInt();
-
-			Log.debug("Factories %d %d %d", factory1, factory2, distance);
-
-		}
-
+		GameInput.initInput(in);
 		// game loop
 		while (true) {
-			Log.debug("inputTurn-----");
-			int entityCount = in.nextInt(); // the number of entities (e.g.
-											// factories and troops)
-			Log.debug("entityCount = %d", entityCount);
-			for (int i = 0; i < entityCount; i++) {
-				int entityId = in.nextInt();
-				String entityType = in.next();
-				int arg1 = in.nextInt();
-				int arg2 = in.nextInt();
-				int arg3 = in.nextInt();
-				int arg4 = in.nextInt();
-				int arg5 = in.nextInt();
-				Log.debug("entity id=%d, type=%s, arg1=%d, arg2=%d, arg3=%d", entityId, entityType, //
-						arg1, arg2, arg3);
-			}
-
-			GameInput.output(OutputAction.WAIT + "");
+			GameInput.turnInput(in);
+			GameInput.turnOutput(Bot.nextAction());
 		}
 	}
 }
@@ -47,16 +17,44 @@ enum OutputAction {
 	MOVE, WAIT
 }
 
+class Bot {
+	static String nextAction() {
+		return OutputAction.WAIT + "";
+	}
+}
+
 class GameInput {
-	static void inputInit(Scanner in) {
-
+	static void initInput(Scanner in) {
+		int factoryCount = in.nextInt(); // the number of factories
+		int linkCount = in.nextInt(); // the number of links between factories
+		Log.debug("factoryCount=%d", factoryCount);
+		Log.debug("linkCount=%d", linkCount);
+		Log.debug("id, id, dist");
+		for (int i = 0; i < linkCount; i++) {
+			int factory1 = in.nextInt();
+			int factory2 = in.nextInt();
+			int distance = in.nextInt();
+			Log.debug("%d %d %d", factory1, factory2, distance);
+		}
 	}
 
-	static void inputTurn(Scanner in) {
-
+	static void turnInput(Scanner in) {
+		int entityCount = in.nextInt();
+		Log.debug("entityCount=%d", entityCount);
+		Log.debug("id, type, arg1, arg2, arg3");
+		for (int i = 0; i < entityCount; i++) {
+			int entityId = in.nextInt();
+			String entityType = in.next();
+			int arg1 = in.nextInt();
+			int arg2 = in.nextInt();
+			int arg3 = in.nextInt();
+			int arg4 = in.nextInt();
+			int arg5 = in.nextInt();
+			Log.debug("%d %s %d %d %d %d %d", entityId, entityType, arg1, arg2, arg3, arg4, arg5);
+		}
 	}
 
-	static void output(String output) {
+	static void turnOutput(String output) {
 		System.out.println(output);
 	}
 }
