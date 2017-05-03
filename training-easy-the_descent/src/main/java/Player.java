01b9499e962ac91a7242cd2ca61e46e7e48e113b
Damien Frémont <damien.fremont@gmail.com>
2017-05-03 16:56:23 +0200

    training-easy-the_descent - first commit

---
+++ training-easy-the_descent/src/main/java/Player.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * The while loop represents the game.
+ * Each iteration represents a turn of the game
+ * where you are given inputs (the heights of the mountains)
+ * and where you have to print an output (the index of the mountain to fire on)
+ * The inputs you are given are automatically updated according to your last actions.
+ **/
+class Player {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+
+        // game loop
+        while (true) {
+            for (int i = 0; i < 8; i++) {
+                int mountainH = in.nextInt(); // represents the height of one mountain.
+            }
+
+            // Write an action using System.out.println()
+            // To debug: System.err.println("Debug messages...");
+
+            System.out.println("4"); // The index of the mountain to fire on.
+        }
+    }
+}
