57ab55e55a7acb3f509d5a96b2c74727db1a6310
Damien Frémont <damien.fremont@gmail.com>
2017-05-05 15:59:28 +0200

    codingame-training-easy-power_of_thor-episode_1 - refactoring code

--- training-easy-power_of_thor-episode_1/src/main/java/Player.java
+++ training-easy-power_of_thor-episode_1/src/main/java/Player.java
@@ -56,20 +56,18 @@ class Model {
 class Bot {
 
 	String next_action(Model model) {
-		String result;
-		Log.debug("Thor position =(%d, %d)", model.thor.x, model.thor.y);
-
 		double angle = angle_between(model.thor, model.light);
 		Model.OutputAction action = get_action_from_angle(angle);
-		update_position(model.thor, action);
-
-		result = action + "";
-		return result;
+		model.thor = update_position(model.thor, action);
+		Log.debug("Thor actual position =(%d, %d)", model.thor.x, model.thor.y);
+		Log.debug("angle_between(thor, light)=%f", angle);
+		return action + "";
 	}
 
-	void update_position(Model.Point thor, Model.OutputAction action) {
+	Model.Point update_position(Model.Point thor, Model.OutputAction action) {
 		thor.x += action.xMove;
 		thor.y += action.yMove;
+		return thor;
 	}
 
 	Model.OutputAction get_action_from_angle(double angle) {
@@ -83,16 +81,11 @@ class Bot {
 	}
 
 	int angle_between(Model.Point p1, Model.Point p2) {
-		int result;
-
 		int difX = p2.x - p1.x;
 		int difY = p2.y - p1.y;
 		double angle = Math.toDegrees(Math.atan2(difX, -difY));
 		angle = angle + Math.ceil(-angle / 360) * 360;
-		result = (int) Math.abs(angle);
-
-		Log.debug("angle_between(thor, light)=%d", result);
-		return result;
+		return (int) Math.abs(angle);
 	}
 
 }
@@ -100,20 +93,17 @@ class Bot {
 class Game {
 
 	Model init(Scanner in, Model model) {
-		Model.Point light = new Model.Point();
-		Model.Point thor = new Model.Point();
-		light.x = in.nextInt();
-		light.y = in.nextInt();
-		thor.x = in.nextInt();
-		thor.y = in.nextInt();
-		model.light = light;
-		model.thor = thor;
+		model.light = new Model.Point();
+		model.light.x = in.nextInt();
+		model.light.y = in.nextInt();
+		model.thor = new Model.Point();
+		model.thor.x = in.nextInt();
+		model.thor.y = in.nextInt();
 		return model;
 	}
 
 	Model turn(Scanner in, Model model) {
-		int remainingTurns = in.nextInt();
-		model.remainingTurns = remainingTurns;
+		model.remainingTurns = in.nextInt();
 		return model;
 	}
 
