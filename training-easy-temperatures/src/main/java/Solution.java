1431367bc86e2d545c9d8c434f45c78b84db6840
Damien Frémont <damien.fremont@gmail.com>
2017-05-07 19:14:45 +0200

    codingame-training-easy-temperatures - add code project

---
+++ training-easy-temperatures/src/main/java/Solution.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt(); // the number of temperatures to analyse
+        if (in.hasNextLine()) {
+            in.nextLine();
+        }
+        String temps = in.nextLine(); // the n temperatures expressed as integers ranging from -273 to 5526
+
+        // Write an action using System.out.println()
+        // To debug: System.err.println("Debug messages...");
+
+        System.out.println("result");
+    }
+}
