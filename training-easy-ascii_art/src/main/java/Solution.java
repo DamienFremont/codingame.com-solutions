da6355e8d16fe755f62f834deb5f318d351d97a6
Damien Frémont <damien.fremont@gmail.com>
2017-05-07 19:34:24 +0200

    codingame-training-easy-ascii_art - first commit, add README, java

---
+++ training-easy-ascii_art/src/main/java/Solution.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int L = in.nextInt();
+        int H = in.nextInt();
+        if (in.hasNextLine()) {
+            in.nextLine();
+        }
+        String T = in.nextLine();
+        for (int i = 0; i < H; i++) {
+            String ROW = in.nextLine();
+        }
+
+        // Write an action using System.out.println()
+        // To debug: System.err.println("Debug messages...");
+
+        System.out.println("answer");
+    }
+}
