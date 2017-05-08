e57f965ea2ae9d06f05cfd9080b2933f2d43e303
Damien Frémont <damien.fremont@gmail.com>
2017-05-08 13:53:44 +0200

    codingame-training-easy-chuck_norris - first commit, add README

---
+++ training-easy-chuck_norris/src/main/java/Solution.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        String MESSAGE = in.nextLine();
+
+        // Write an action using System.out.println()
+        // To debug: System.err.println("Debug messages...");
+
+        System.out.println("answer");
+    }
+}
