6b55ee07cb97ee401757137dd682c94a571173f0
Damien Frémont <damien.fremont@gmail.com>
2017-05-09 18:46:07 +0200

    codingame-training-easy-horse_racing_duals - first commit

---
+++ training-easy-horse_racing_duals/src/main/java/Player.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        for (int i = 0; i < N; i++) {
+            int pi = in.nextInt();
+        }
+
+        // Write an action using System.out.println()
+        // To debug: System.err.println("Debug messages...");
+
+        System.out.println("answer");
+    }
+}
