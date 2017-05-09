5296114c8e3c11766aaae0d8d9fb42231a490a1c
Damien Frémont <damien.fremont@gmail.com>
2017-05-09 15:13:04 +0200

    codingame-training-easy-defibrillators - first commit

---
+++ training-easy-defibrillators/src/main/java/Solution.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse the standard input
+ * according to the problem statement.
+ **/
+class Solution {
+
+	public static void main(String args[]) {
+		Scanner in = new Scanner(System.in);
+		String LON = in.next();
+		String LAT = in.next();
+		int N = in.nextInt();
+		if (in.hasNextLine()) {
+			in.nextLine();
+		}
+		for (int i = 0; i < N; i++) {
+			String DEFIB = in.nextLine();
+		}
+
+		// Write an action using System.out.println()
+		// To debug: System.err.println("Debug messages...");
+
+		System.out.println("answer");
+	}
+}
