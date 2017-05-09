52df59767843dfd974e373247e36d46ccd8a654c
Damien Frémont <damien.fremont@gmail.com>
2017-05-09 18:42:15 +0200

    codingame-training-easy-training-easy-mars_lander_episode_1 - solved!

--- training-easy-mars_lander_episode_1/src/main/java/Player.java
+++ training-easy-mars_lander_episode_1/src/main/java/Player.java
@@ -1,37 +1,35 @@
-import java.util.*;
-import java.io.*;
-import java.math.*;
+import java.util.Scanner;
 
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
 class Player {
 
-    public static void main(String args[]) {
-        Scanner in = new Scanner(System.in);
-        int surfaceN = in.nextInt(); // the number of points used to draw the surface of Mars.
-        for (int i = 0; i < surfaceN; i++) {
-            int landX = in.nextInt(); // X coordinate of a surface point. (0 to 6999)
-            int landY = in.nextInt(); // Y coordinate of a surface point. By linking all the points together in a sequential fashion, you form the surface of Mars.
-        }
+	public static void main(String args[]) {
+		Scanner in = new Scanner(System.in);
+		int surfaceN = in.nextInt();
+		for (int i = 0; i < surfaceN; i++) {
+			int landX = in.nextInt();
+			int landY = in.nextInt();
+		}
 
-        // game loop
-        while (in.hasNext()) {
-            int X = in.nextInt();
-            int Y = in.nextInt();
-            int hSpeed = in.nextInt(); // the horizontal speed (in m/s), can be negative.
-            int vSpeed = in.nextInt(); // the vertical speed (in m/s), can be negative.
-            int fuel = in.nextInt(); // the quantity of remaining fuel in liters.
-            int rotate = in.nextInt(); // the rotation angle in degrees (-90 to 90).
-            int power = in.nextInt(); // the thrust power (0 to 4).
+		while (in.hasNext()) {
+			int X = in.nextInt();
+			int Y = in.nextInt();
+			int hSpeed = in.nextInt();
+			int vSpeed = in.nextInt();
+			int fuel = in.nextInt();
+			int rotate = in.nextInt();
+			int power = in.nextInt();
 
-            // Write an action using System.out.println()
-            // To debug: System.err.println("Debug messages...");
-
-
-            // 2 integers: rotate power. rotate is the desired rotation angle (should be 0 for level 1), power is the desired thrust power (0 to 4).
-            System.out.println("0 3");
-        }
-    }
+			int outRotate = 0;
+			int outPower = 0;
+			if (1500 < Y && Y < 3000 && vSpeed < -30)
+				outPower = 4;
+			else if (1000 < Y && Y < 1500 && vSpeed < -20)
+				outPower = 4;
+			else if (500 < Y && Y < 1000 && vSpeed < -10)
+				outPower = 4;
+			else if (0 < Y && Y < 500 && vSpeed < 0)
+				outPower = 4;
+			System.out.println(String.format("%d %d", outRotate, outPower));
+		}
+	}
 }
