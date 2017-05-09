fbee85d5744eb32f1b4ca7a5c002f56efc5c3302
Damien Frémont <damien.fremont@gmail.com>
2017-05-09 14:32:38 +0200

    codingame-multiplayer-codegolf-temperatures - first commit

---
+++ multiplayer-codegolf-temperatures/src/test/java/MinifierTest.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.Test;
+
+public class MinifierTest {
+
+	@Test
+	public void test_1() throws IOException {
+		String argument = getPath("PlayerToMinify.java");
+		Minifier.minify(argument);
+	}
+
+	private String getPath(String resourceName) throws IOException {
+		ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+		File file = new File(classLoader.getResource(resourceName).getFile());
+		return file.toPath().toString();
+	}
+}
