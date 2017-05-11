a91b7afdf885633b00037edec13a3254d43606ce
Damien Frémont <damien.fremont@gmail.com>
2017-05-11 15:30:48 +0200

    codingame-training-medium-skynet_revolution_episode_1 - solved case 01

--- projects/training-medium-skynet_revolution_episode_1/src/main/java/Player.java
+++ projects/training-medium-skynet_revolution_episode_1/src/main/java/Player.java
@@ -1,36 +1,98 @@
-import java.util.*;
-import java.io.*;
-import java.math.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
 class Player {
+	public static void main(String args[]) {
+		Scanner in = new Scanner(System.in);
+		Input model = new Input();
+		model.init = Game.init(in);
+		while (in.hasNext()) {
+			model.turn = Game.turn(in);
+			model.output = Bot.solve(model);
+			Game.play(model.output);
+		}
+	}
+}
 
-    public static void main(String args[]) {
-        Scanner in = new Scanner(System.in);
-        int N = in.nextInt(); // the total number of nodes in the level, including the gateways
-        int L = in.nextInt(); // the number of links
-        int E = in.nextInt(); // the number of exit gateways
-        for (int i = 0; i < L; i++) {
-            int N1 = in.nextInt(); // N1 and N2 defines a link between these nodes
-            int N2 = in.nextInt();
-        }
-        for (int i = 0; i < E; i++) {
-            int EI = in.nextInt(); // the index of a gateway node
-        }
+class Input {
+	Init init;
+	Turn turn;
+	Output output;
 
-        // game loop
-        while (true) {
-            int SI = in.nextInt(); // The index of the node on which the Skynet agent is positioned this turn
+	static class Init {
 
-            // Write an action using System.out.println()
-            // To debug: System.err.println("Debug messages...");
+		int N, L, E;
+		List<Link> links = new ArrayList<>();
+		List<Integer> indexes = new ArrayList<>();
 
+		static class Link {
+			int N1, N2;
+		}
+	}
 
-            // Example: 0 1 are the indices of the nodes you wish to sever the link between
-            System.out.println("0 1");
-        }
-    }
+	static class Turn {
+		int SI;
+	}
+
+	static class Output {
+		int C1, C2;
+	}
+}
+
+class Bot {
+	static Input.Output solve(Input model) {
+		Input.Output output = new Input.Output();
+		output.C1 = 1;
+		output.C2 = 2;
+		return output;
+	}
+}
+
+class Game {
+	static Input.Init init(Scanner in) {
+		Input.Init model = new Input.Init();
+		model.N = in.nextInt();
+		model.L = in.nextInt();
+		model.E = in.nextInt();
+		Log.debug("%d %d %d", model.N, model.L, model.E);
+		Input.Init.Link link = new Input.Init.Link();
+		for (int i = 0; i < model.L; i++) {
+			link.N1 = in.nextInt();
+			link.N2 = in.nextInt();
+			model.links.add(link);
+			Log.debug("%d %d", link.N1, link.N2);
+		}
+		int EI;
+		for (int i = 0; i < model.E; i++) {
+			EI = in.nextInt();
+			model.indexes.add(EI);
+			Log.debug("%d %d", link.N1, link.N2);
+		}
+		return model;
+	}
+
+	static Input.Turn turn(Scanner in) {
+		Input.Turn model = new Input.Turn();
+		model.SI = in.nextInt();
+		Log.debug("%d", model.SI);
+		return model;
+	}
+
+	static void play(Input.Output output) {
+		System.out.println(String.format("%s %s", output.C1, output.C2));
+	}
+}
+
+class Log {
+	static void debug(String pattern, Object... values) {
+		System.err.println(String.format(pattern, values));
+	}
+}
+
+class Preconditions {
+	static void check(boolean condition) {
+		if (!condition)
+			throw new RuntimeException("CONDITION FALSE");
+	}
 }
