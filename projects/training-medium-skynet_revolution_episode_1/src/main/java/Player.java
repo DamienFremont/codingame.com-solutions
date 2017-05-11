34c0c133e69c4b8e04546da3ceb8076bac047045
Damien Frémont <damien.fremont@gmail.com>
2017-05-11 15:54:38 +0200

    codingame-training-medium-skynet_revolution_episode_1 - solved case 02

--- projects/training-medium-skynet_revolution_episode_1/src/main/java/Player.java
+++ projects/training-medium-skynet_revolution_episode_1/src/main/java/Player.java
@@ -5,81 +5,81 @@ import java.util.Scanner;
 class Player {
 	public static void main(String args[]) {
 		Scanner in = new Scanner(System.in);
-		Input model = new Input();
-		model.init = Game.init(in);
+		Model model = Game.init(in);
 		while (in.hasNext()) {
-			model.turn = Game.turn(in);
-			model.output = Bot.solve(model);
-			Game.play(model.output);
+			model = Game.turn(in, model);
+			model = Bot.solve(model);
+			Game.play(model);
 		}
 	}
 }
 
-class Input {
-	Init init;
-	Turn turn;
-	Output output;
+class Model {
+	int N, L, E, SI, C1, C2;
+	List<Link> links;
+	List<Integer> gateways;
 
-	static class Init {
-
-		int N, L, E;
-		List<Link> links = new ArrayList<>();
-		List<Integer> indexes = new ArrayList<>();
-
-		static class Link {
-			int N1, N2;
-		}
-	}
-
-	static class Turn {
-		int SI;
-	}
-
-	static class Output {
-		int C1, C2;
+	static class Link {
+		int N1, N2;
+		boolean cutted;
 	}
 }
 
 class Bot {
-	static Input.Output solve(Input model) {
-		Input.Output output = new Input.Output();
-		output.C1 = 1;
-		output.C2 = 2;
-		return output;
+	static Model solve(Model model) {
+		int EI = nearest_gateway(model);
+		Model.Link link = find_link(model, EI);
+		link.cutted = true;
+		model.C1 = link.N1;
+		model.C2 = link.N2;
+		return model;
+	}
+
+	static Model.Link find_link(Model model, int EI) {
+		return model.links.stream() //
+				.filter(i -> (EI == i.N1 || EI == i.N2) && i.cutted == false) //
+				.findFirst().get();
+	}
+
+	static int nearest_gateway(Model model) {
+		return model.gateways.get(0);
 	}
 }
 
 class Game {
-	static Input.Init init(Scanner in) {
-		Input.Init model = new Input.Init();
+	static Model init(Scanner in) {
+		Log.debug("init");
+		Model model = new Model();
 		model.N = in.nextInt();
 		model.L = in.nextInt();
 		model.E = in.nextInt();
 		Log.debug("%d %d %d", model.N, model.L, model.E);
-		Input.Init.Link link = new Input.Init.Link();
+		model.links = new ArrayList<>();
 		for (int i = 0; i < model.L; i++) {
+			Model.Link link = new Model.Link();
 			link.N1 = in.nextInt();
 			link.N2 = in.nextInt();
 			model.links.add(link);
 			Log.debug("%d %d", link.N1, link.N2);
 		}
+		model.gateways = new ArrayList<>();
 		int EI;
 		for (int i = 0; i < model.E; i++) {
 			EI = in.nextInt();
-			model.indexes.add(EI);
-			Log.debug("%d %d", link.N1, link.N2);
+			model.gateways.add(EI);
+			Log.debug("%d", EI);
 		}
 		return model;
 	}
 
-	static Input.Turn turn(Scanner in) {
-		Input.Turn model = new Input.Turn();
+	static Model turn(Scanner in, Model model) {
+		Log.debug("turn");
 		model.SI = in.nextInt();
 		Log.debug("%d", model.SI);
 		return model;
 	}
 
-	static void play(Input.Output output) {
+	static void play(Model output) {
 		System.out.println(String.format("%s %s", output.C1, output.C2));
 	}
 }
@@ -87,12 +87,5 @@ class Game {
 class Log {
 	static void debug(String pattern, Object... values) {
 		System.err.println(String.format(pattern, values));
-	}
-}
-
-class Preconditions {
-	static void check(boolean condition) {
-		if (!condition)
-			throw new RuntimeException("CONDITION FALSE");
 	}
 }
