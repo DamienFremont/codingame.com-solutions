549c18a5b5bc1c9f3820d47a4133d2cb4808ff88
Damien Frémont <damien.fremont@gmail.com>
2017-05-23 20:16:46 +0200

    codingame-training-medium-stock_exchange_losses - first commit

---
+++ training-medium-stock_exchange_losses/src/main/java/Solution.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for (int i = 0; i < n; i++) {
+            int v = in.nextInt();
+        }
+
+        // Write an action using System.out.println()
+        // To debug: System.err.println("Debug messages...");
+
+        System.out.println("answer");
+    }
+}
