856f1b4353ed80f3ee77226d89bc86b90a31cbf3
Damien Frémont <damien.fremont@gmail.com>
2017-05-23 20:24:43 +0200

    codingame-training-medium-stock_exchange_losses - implemented tests

--- training-medium-stock_exchange_losses/src/main/java/Solution.java
+++ training-medium-stock_exchange_losses/src/main/java/Solution.java
@@ -1,23 +1,94 @@
-import java.util.*;
-import java.io.*;
-import java.math.*;
+import java.util.Scanner;
 
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
 class Solution {
 
-    public static void main(String args[]) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        for (int i = 0; i < n; i++) {
-            int v = in.nextInt();
-        }
+	public static void main(String args[]) {
+		Env.read(args);
 
-        // Write an action using System.out.println()
-        // To debug: System.err.println("Debug messages...");
+		Scanner in = new Scanner(System.in);
+		Model model = Game.init(in);
 
-        System.out.println("answer");
-    }
+		System.out.println("answer");
+	}
+}
+
+class Model {
+	int[] stockValues;
+}
+
+class Game {
+	static Model init(Scanner in) {
+		Log.debug("INIT =======================");
+		Model m = new Model();
+		int n = in.nextInt();
+		Log.info("%d", n);
+		m.stockValues = new int[n];
+		String vs = "";
+		for (int i = 0; i < n; i++) {
+			int v = in.nextInt();
+			m.stockValues[i] = n;
+			vs += v + " ";
+		}
+
+		Log.info("%s", vs);
+		return m;
+	}
+}
+// UTILS
+
+class Log {
+	static boolean Level_DEBUG;
+
+	static void info(String pattern, Object... values) {
+		log(pattern, values);
+	}
+
+	static void debug(String pattern, Object... values) {
+		if (!Level_DEBUG)
+			return;
+		log(pattern, values);
+	}
+
+	private static void log(String pattern, Object... values) {
+		System.err.println(String.format(pattern, values));
+	}
+}
+
+class Env {
+
+	static void read(String args[]) {
+		Log.Level_DEBUG = false;
+		if (args == null)
+			return;
+		for (String arg : args) {
+			if ("-debug".equals(arg))
+				Log.Level_DEBUG = true;
+		}
+	}
+}
+
+class Point {
+	int x, y;
+
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
+
+class Vector {
+	double x, y;
+
+	public Vector(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+}
+
+class Preconditions {
+
+	static void check(boolean condition) {
+		if (!condition)
+			throw new RuntimeException("CONDITION FALSE");
+	}
 }
---
+++ training-medium-stock_exchange_losses/src/test/java/SolutionTest.java
@@ -0,0 +1,30 @@
+import org.junit.Test;
+
+public class SolutionTest {
+
+	@Test
+	public void test_case_01() {
+		TestCase.execute("01", () -> Solution.main(new String[] { "-debug" }));
+	}
+
+	@Test
+	public void test_case_02() {
+		TestCase.execute("02", () -> Solution.main(new String[] { "-debug" }));
+	}
+
+	@Test
+	public void test_case_03() {
+		TestCase.execute("03", () -> Solution.main(new String[] { "-debug" }));
+	}
+
+	@Test
+	public void test_case_04() {
+		TestCase.execute("04", () -> Solution.main(new String[] { "-debug" }));
+	}
+
+	@Test
+	public void test_case_06() {
+		TestCase.execute("06", () -> Solution.main(new String[] { "-debug" }));
+	}
+
+}
